760d478c9cfb0e2a523f91802eb799e2ce0c907f
ruchita20 <ruchita@berkeley.edu>
2014-07-09 12:31:56 -0700

    adding JS events

--- templates/main.php
+++ templates/main.php
@@ -1,9 +1,17 @@
+<?php 
+\OCP\Util::addScript('readlater', 'script');  
+\OCP\Util::addScript('readlater', 'bootstrap');  
+\OCP\Util::addScript('readlater', 'bootstrap.min'); 
 
+\OCP\Util::addStyle('readlater', 'style'); 
+\OCP\Util::addStyle('readlater', 'bootstrap');  
+
+?>
 
 <div id="app">
 	<div id="app-navigation">
 		<ul class="with-icon">
-		<?php print_unescaped($this->inc('part.addnew')) ?>
+		<?php print_unescaped($this->inc('part.add')) ?>
 <?php print_unescaped($this->inc('part.feed.unread')) ?>	
 			<?php print_unescaped($this->inc('part.search')) ?>
 		<?php print_unescaped($this->inc('part.allitems')) ?>
---
+++ templates/part.add.php
@@ -0,0 +1,20 @@
+<li class="add-new">
+        <a class="list-title list-title-with-icon">+ <span><?php p($l->t('Add Website'))?></span></a>
+
+        <div class="add-new-popup">
+
+                <fieldset class="personalblock">
+
+                        <form>
+
+                                <input type="text" 
+                                        placeholder="<?php p($l->t('Address')); ?>" 
+                                        name="adress"
+                                        autofocus>
+                                <button title="<?php p($l->t('Add')); ?>" 
+                                                class="primary"><?php p($l->t('Add')); ?></button>
+                        </form>
+                        
+                </fieldset>
+        </div>
+</li>
