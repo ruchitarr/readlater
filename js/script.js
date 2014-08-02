26adc34fc19aa288efc47c1cb0dcd89fb45a048a
ruchita20 <ruchita@berkeley.edu>
2014-08-02 02:05:37 -0700

    OC.generateUrl for images

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
 
 (function ($, OC) {
 	var items = [];
-	var imgStarUrl = OC.generateUrl('./apps/readlater/img/star.png');
+	var imgStarUrl = OC.generateUrl('/apps/readlater/img/star.png');
 
 	$(document).ready(function () {
 		$("div#addContent").hide();
@@ -80,7 +80,7 @@ function showData(){
 
 	$.each(msg, function(i, item) {
 
-          items.push('<li><div class="title"><a class="bookmrk" href="item.url">' + item.url + '</a><br/><a class="list-title list-title-with-icon star-icon"></a><a class="list-title list-title-with-icon edit-icon"> </a><a class="list-title list-title-with-icon delete-icon"> </a></div>  </li>');
+          items.push('<li><div class="title"><a class="bookmrk" href="item.url">' + item.url + '</a><br/><a class="list-title list-title-with-icon star-icon"><img src="imgStarUrl"></a><a class="list-title list-title-with-icon edit-icon"> </a><a class="list-title list-title-with-icon delete-icon"> </a></div>  </li>');
 
   alert(item.url);
 	
