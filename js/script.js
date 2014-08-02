2ff58067f1387323e3372691ae76b7651b4632fc
ruchita20 <ruchita@berkeley.edu>
2014-08-02 02:14:13 -0700

    OC.generateUrl for images

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ function showData(){
 
 	$.each(msg, function(i, item) {
 
-          items.push('<li><div class="title"><a class="bookmrk" href="item.url">' + item.url + '</a><br/><a class="list-title list-title-with-icon star-icon"><img src="imgStarUrl"></a><a class="list-title list-title-with-icon edit-icon"> </a><a class="list-title list-title-with-icon delete-icon"> </a></div>  </li>');
+          items.push('<li><div class="title"><a class="bookmrk" href="item.url">' + item.url + '</a><br/><a class="list-title list-title-with-icon"><img src="imgStarUrl"></a><a class="list-title list-title-with-icon edit-icon"> </a><a class="list-title list-title-with-icon delete-icon"> </a></div>  </li>');
 
   alert(item.url);
 	
