2f235159e73cd5e4a938441934bcc2301f674153
ruchita20 <ruchita@berkeley.edu>
2014-08-06 02:47:36 -0700

    delete function

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@
 		}).done(function( msg ) {
 
 		$.each(msg, function(i, item) {
-			items.push('<li><div class="title"><a class="bookmrk" href="item.url" id="item.id">' + item.url + '</a><br/><a class="list-title list-title-with-icon icon icon-star">&nbsp; </a>&nbsp;<a class="list-title list-title-with-icon icon icon-rename">&nbsp;  </a><a class="list-title list-title-with-icon icon icon-delete onclick="removeItem()"">&nbsp;  </a></div>  </li>');
+			items.push('<li><div class="title"><a class="bookmrk" href="item.url" id="item.id">' + item.url + '</a><br/><a class="list-title list-title-with-icon icon icon-star">&nbsp; </a>&nbsp;<a class="list-title list-title-with-icon icon icon-rename">&nbsp;  </a><a class="list-title list-title-with-icon icon icon-delete" onclick="removeItem()">&nbsp;  </a></div>  </li>');
  			console.log(item.url);
 		});  // close each()
 		$('#listfeedUL').append( items.join('') );
@@ -84,8 +84,8 @@
 
 	}
 	//remove item
-	$(document).on('click','a .icon-delete', function(){
-		alert('I was clicked');
+	$(document).on('click','a.icon-delete', function(){
+		alert('Delete item clicked');
 		removeItem();
 	});
 
