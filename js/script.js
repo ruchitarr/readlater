7f678bdceae3fc01ee875c2f1ce7d946eab7c92e
brantje <brantje@gmail.com>
2014-08-10 12:57:27 +0200

    Item description to new line

--- js/script.js
+++ js/script.js
@@ -65,7 +65,7 @@
 
 		
 		$.each(msg, function(i, item) {
-			items.push('<li data-itemId="'+ item.id +'"><div class="title"><a class="bookmrk" href="'+ item.url +'" target="_blank" id="" title="'+ item.url +'">' + item.url + item.description+ '</a><br/><a class="list-title list-title-with-icon icon icon-star"></a>&nbsp;<a class="list-title list-title-with-icon icon icon-rename"></a><a class="list-title list-title-with-icon icon icon-delete"></a></div>  </li>');
+			items.push('<li data-itemId="'+ item.id +'"><div class="title"><a class="bookmrk" href="'+ item.url +'" target="_blank" id="" title="'+ item.url +'">' + item.title +  '</a> <div class="itemDesc">'+ item.description+ '</div><br/><a class="list-title list-title-with-icon icon icon-star"></a>&nbsp;<a class="list-title list-title-with-icon icon icon-rename"></a><a class="list-title list-title-with-icon icon icon-delete"></a></div>  </li>');
  			console.log(item);
 		});  // close each()
 		$('#listfeedUL').append( items.join('') );
